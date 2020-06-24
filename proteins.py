546c22d0783feea3e0d4400494aca0521a1e0ac6
callum-mccracken <callum189@gmail.com>
2020-06-23 20:25:19 -0400

    initial add of sketchy code

---
+++ proteins.py
@@ -0,0 +1,176 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+from tqdm import tqdm
+import os
+
+seq_str = "PHPPPHPHPPPH"
+#seq_str = "PHPPPH"
+
+# ensure this only contians Ps and Hs!
+sequence = np.array(list(seq_str))
+
+if not os.path.exists(seq_str):
+    os.mkdir(seq_str)
+
+n_monomers = len(sequence)
+
+# 0 for H, 1 for P
+binary_sequence = np.zeros(n_monomers)
+binary_sequence[sequence=="P"] = 1
+
+def check_good(move_list):
+    # move_list = [[0,1], ...] list of moves on a grid.
+
+    # number of times we've reached each point on the grid
+    grid = np.zeros((n_monomers*2, n_monomers*2), dtype=int)
+
+    # start at the middle
+    x = n_monomers
+    y = n_monomers
+    grid[x,y] = 1
+
+    points = [[x,y]]
+    for dx, dy in move_list:
+        x += dx
+        y += dy
+        if grid[x,y] == 1:
+            # self-intersection!
+            return False, points
+        grid[x,y] += 1
+        points.append([x,y])
+
+    # if we haven't returned, no self-intersections yet!
+    
+    # still have to check compactness though
+    pts_reached = np.where(grid==1)
+    min_x = min(pts_reached[0])
+    max_x = max(pts_reached[0])
+    min_y = min(pts_reached[1])
+    max_y = max(pts_reached[1])
+    # make compact array
+    compact = np.zeros_like(grid)
+    compact[min_x:max_x+1, min_y:max_y+1] = 1
+    if not np.array_equal(compact, grid):  # not compact
+        return False, grid
+    return True, points
+
+def plot(move_list, energy_penalty=None, n=None, show=False):
+    plt.cla()
+    plt.clf()
+    plt.axis('equal')
+    #plt.xlim(-10,10)
+    #plt.ylim(-10,10)
+
+    gray = "#CACACA"
+    red = "#C46B6B"
+
+    clist = [red if b == 0 else gray for b in binary_sequence]
+    
+    # start at the origin
+    x = 0
+    y = 0
+    # color = green for the first dot, just for clarity
+    plt.plot([x], [y], color='g', marker='o')
+    for i, (dx, dy) in enumerate(move_list):
+        plt.plot([x,x+dx], [y,y+dy], 'k')
+        x += dx
+        y += dy
+        plt.plot([x], [y], color=clist[i+1], marker='o')
+    if energy_penalty is not None:
+        plt.title(f"$\\varepsilon={energy_penalty}$")
+
+    plt.savefig(f'{seq_str}/plot{n}.png')
+    if show:
+        plt.show()
+
+
+possible_moves = [[0,1], [0,-1], [1,0], [-1,0]]
+n_moves = n_monomers-1
+
+print("finding all possible conformations")
+points_arr = []
+moves_arr = []
+for moves in tqdm([m for m in itertools.product(possible_moves, repeat=n_moves-1)]):
+    # make sure the first move is [0,1] (up)
+    moves = [[0,1]] + list(moves)
+    # ensure first turn is right!
+    if [1,0] in moves:
+        r_index = moves.index([1,0])
+        if [-1,0] in moves:
+            l_index = moves.index([-1,0])
+            if r_index > l_index:
+                continue  # this ensures the first turn is right
+    else:
+        continue
+
+    is_good, points = check_good(moves)
+    if is_good:  # if compact and no self-intersections
+        points_arr.append(points)
+        moves_arr.append(moves)
+
+print("found", len(points_arr))
+
+def energy_penalty(type1, type2):
+    # recall 0 for H, 1 for P
+    if type1 == 0 and type2 == 0:
+        # H--H has energy penalty 0
+        return 0
+    elif type1 == 0 and type2 == 1:
+        # H--P has energy penalty 1
+        return 1
+    elif type1 == 1 and type2 == 0:
+        # P--H has energy penalty 1, but to avoid double-counting say it's 0
+        return 0
+    elif type1 == 1 and type2 == 1:
+        # P--P has energy penalty 0, but to avoid double-counting say it's 0
+        return 0
+    else:
+        raise ValueError("why are you here?")
+
+
+def total_energy_penalty(points, types):
+    # types[i] = 0 or 1, for H or P respectively
+    penalty = 0
+    for i, point in enumerate(points):
+        point_x, point_y = point
+        if [point_x, point_y+1] in points:  # check up
+            j = points.index([point_x, point_y+1])
+            penalty += energy_penalty(types[i],types[j])
+        else:
+            penalty += 1 if types[i] == 0 else 0
+        if [point_x, point_y-1] in points: # down
+            j = points.index([point_x, point_y-1])
+            penalty += energy_penalty(types[i],types[j])
+        else:
+            penalty += 1 if types[i] == 0 else 0
+        if [point_x+1, point_y] in points: # right
+            j = points.index([point_x+1, point_y])
+            penalty += energy_penalty(types[i],types[j])
+        else:
+            penalty += 1 if types[i] == 0 else 0
+        if [point_x-1, point_y] in points: # left
+            j = points.index([point_x-1, point_y])
+            penalty += energy_penalty(types[i],types[j])
+        else:
+            penalty += 1 if types[i] == 0 else 0
+
+    return penalty
+
+print("calculating energy penalties")
+eps = np.zeros(len(points_arr))
+for i, pts in tqdm(enumerate(points_arr)):
+    eps[i] = total_energy_penalty(pts, binary_sequence)
+
+for i in range(len(moves_arr)):
+    show = (eps[i] == min(eps))
+    plot(moves_arr[i], eps[i], i, show=show)
+
+
+print("min energy penalty:", min(eps))
+print("degeneracy:", len(np.where(eps==min(eps))[0]))
+
+plt.cla()
+plt.clf()
+plt.hist(eps)
+plt.show()
